2e8da87dab787afa29bf42ace2e6a7d6b3626351
Alex Boyd <alexanderleeboyd@gmail.com>
2022-08-09 14:00:44 -0400

    Layout: admin/invoice/show page

--- app/controllers/admin/invoices_controller.rb
+++ app/controllers/admin/invoices_controller.rb
@@ -9,7 +9,18 @@ class Admin::InvoicesController < ApplicationController
 
   def update
     invoice = Invoice.find(params[:id])
-    invoice.update(status: params[:status])
+    if params[:status] == "In Progress"
+      invoice.update(status: "in_progress")
+    end
+
+    if params[:status] == "Cancelled"
+      invoice.update(status: "cancelled")
+    end
+
+    if params[:status] == "Completed"
+      invoice.update(status: "completed")
+    end
+    
     redirect_to "/admin/invoices/#{params[:id]}"
   end
 end
